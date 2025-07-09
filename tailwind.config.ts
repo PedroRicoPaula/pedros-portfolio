9667738a06c7e8e200906bfbd948d1c0bc008bfe
Pedro Paula <136763444+PedroRicoPaula@users.noreply.github.com>
2025-07-09 11:47:26 +0000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,7 +102,7 @@ export default {
 				'accordion-down': 'accordion-down 0.2s ease-out',
     				'accordion-up': 'accordion-up 0.2s ease-out',
                 		// ADICIONE ESTA ANIMAÇÃO 'snow' AQUI
-                		'snow': 'snow linear infinite'
+                		'snow': 'snow 15s linear infinite'
 			}
 		}
 	},
